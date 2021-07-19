3e6f44e077fa8a94d595dabb622d472b3a06606c
43695512 <yanter@wp.pl>
2021-07-19 22:59:11 +0200

    Cloudinary image database

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,12 +5,16 @@ const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
-// const campground = require('../models/campground');
 const campgroundController = require('../controllers/campgrounds')
+const multer = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
+
 
 router.route('/')
     .get(catchAsync(campgroundController.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgroundController.createCampground))
+    // .post(isLoggedIn, validateCampground, catchAsync(campgroundController.createCampground))
+    .post(upload.array('campground[image]'), (req, res) => { console.log(req.body, req.files); res.send('Worked!') })
 
 router.get('/new', isLoggedIn, campgroundController.renderNewForm)
 
